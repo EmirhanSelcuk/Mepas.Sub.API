7f9c8d6510e2d43b8102e9a412eb4694a8600947
emirhanselcuk <selcukemirhan88@gmail.com>
2024-12-10 09:34:48 +0300

    Add ExceptionMiddleware

---
+++ Mepas.Sub.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,36 @@
+﻿namespace Mepas.Sub.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                // Hata yakalama işlemleri burada yapılabilir
+                _logger.LogError(ex, "bir hata olustu");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.StatusCode = 500;
+            return context.Response.WriteAsync("Bir hata olustu.");
+        }
+    }
+}
+
