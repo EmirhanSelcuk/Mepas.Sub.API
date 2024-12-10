b5b55eb5b6942e23b1d0330e9e8f93a02787bc00
emirhanselcuk <selcukemirhan88@gmail.com>
2024-12-10 09:34:33 +0300

    Add CreateSubscriber

--- Mepas.Sub.API/Services/SubscribeService.cs
+++ Mepas.Sub.API/Services/SubscribeService.cs
@@ -10,10 +10,24 @@ public class SubscriberService : ISubscriberService
         _subscriberRepository = subscriberRepository;
     }
 
+
     public async Task<Result<List<Subscriber>>> GetSubscribersAsync(DateTime KayitBaslangic, DateTime KayitBitis, DateTime AranmaBaslangic, DateTime AranmaBitis)
     {
         // Veritabanından aboneleri al
         var subscribers = await _subscriberRepository.GetSubscribersAsync(KayitBaslangic, KayitBitis, AranmaBaslangic, AranmaBitis);
         return new Result<List<Subscriber>> { IsSuccess = true, Data = subscribers };
+    }
+
+    public async Task<Result<Subscriber>> CreateSubscriberAsync(Subscriber subscriber)
+    {
+        try
+        {
+            await _subscriberRepository.CreateSubscriberAsync(subscriber); // Veritabanına ekleme işlemi
+            return new Result<Subscriber> { Data = subscriber, IsSuccess = true };
+        }
+        catch (Exception ex)
+        {
+            return new Result<Subscriber> { IsSuccess = false, Message = ex.Message };
+        }
     }
 }
